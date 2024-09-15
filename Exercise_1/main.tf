a7779fd28b4f68e8519fdcfd13e6e87acdb34aec
dpt1305 <tungplatin@gmail.com>
2024-09-15 10:38:12 +0700

    refactor code

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,17 +1,17 @@
 # TODO: Designate a cloud provider, region, and credentials
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.0"
+        }
     }
-  }
 }
 
 provider "aws" {
-  region                   = "us-east-1"
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "udacity"
+    region                   = "us-east-1"
+    shared_credentials_files = ["~/.aws/credentials"]
+    profile                  = "udacity"
 }
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
@@ -62,23 +62,23 @@ resource "aws_instance" "micro_instance_4" {
 
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
-# resource "aws_instance" "large_instance_1" {
-#     ami = "ami-0182f373e66f89c85"  ## Amazon Linux 2023 AMI
-#     instance_type = "m4.large"
-#     subnet_id = "subnet-0c53ad4ad49f2cfe3"
-#     key_name = resource.aws_key_pair.udacity_key.key_name
-#     tags = {
-#         Name = "Udacity M4"
-#         Project = "udacity_pjt_2"
-#     }
-# }
-# resource "aws_instance" "large_instance_2" {
-#     ami = "ami-0182f373e66f89c85"  ## Amazon Linux 2023 AMI
-#     instance_type = "m4.large"
-#     subnet_id = "subnet-0c53ad4ad49f2cfe3"
-#     key_name = resource.aws_key_pair.udacity_key.key_name
-#     tags = {
-#         Name = "Udacity M4"
-#         Project = "udacity_pjt_2"
-#     }
-# }
+resource "aws_instance" "large_instance_1" {
+    ami = "ami-0182f373e66f89c85"  ## Amazon Linux 2023 AMI
+    instance_type = "m4.large"
+    subnet_id = "subnet-0c53ad4ad49f2cfe3"
+    key_name = resource.aws_key_pair.udacity_key.key_name
+    tags = {
+        Name = "Udacity M4"
+        Project = "udacity_pjt_2"
+    }
+}
+resource "aws_instance" "large_instance_2" {
+    ami = "ami-0182f373e66f89c85"  ## Amazon Linux 2023 AMI
+    instance_type = "m4.large"
+    subnet_id = "subnet-0c53ad4ad49f2cfe3"
+    key_name = resource.aws_key_pair.udacity_key.key_name
+    tags = {
+        Name = "Udacity M4"
+        Project = "udacity_pjt_2"
+    }
+}
